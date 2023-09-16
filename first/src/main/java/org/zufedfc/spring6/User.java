2b4c2aed0cd20b8c01c50aaebb3286473d63a9c1
zsy <530822987@qq.com>
2023-09-16 09:40:29 +0800

    111

---
+++ first/src/main/java/org/zufedfc/spring6/User.java
@@ -0,0 +1,14 @@
+package org.zufedfc.spring6;
+
+public class User {
+
+    private String name;
+
+    public void add(){
+        System.out.println("add...");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ first/src/test/java/TestUser.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.zufedfc.spring6.User;
+
+public class TestUser {
+
+    @Test
+    public void testAdd(){
+//        加载spring配置文件
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+//        获取配置创建的对象
+        User user = (User) applicationContext.getBean("user");
+        user.add();
+    }
+
+
+}
